33d6aaa7d3d421e2a7e7206e975f9c4f8b6fa5f4
Oleg <garrbuzz@gmail.com>
2018-09-30 22:25:40 +0300

    3009_2018

--- php/sendMessage.php
+++ php/sendMessage.php
@@ -7,17 +7,16 @@ include 'functions.php';
 $userName = $_POST['userName'];
 $userMail = $_POST['userMail'];
 $textMessege = $_POST['messageText'];
-$textMessege .= "Content-type: text/html; charset=\"windows-1251\"\n";
-$textMessege .= "Content-Transfer-Encoding: 8bit\n\n";
+
 $from = "site@voloh.org.ua\r\n";
-$subject = 'Message from site from   ';
+$subject = 'Повідомлення з сайту від   ';
 $subject .= $userName;
 
-$headers  = "MIME-Version: 1.0";
-$headers .= "Content-type: text/html; charset=windows-1251" . "\r\n";
+$headers  = "MIME-Version: 1.0\r\n";
+$headers .= "Content-type: text/html; charset=\"utf-8\"\n";
 $headers .= "From: $from";
 
-$send = mail("voloh@i.ua ", $subject, $textMessege, $headers);
+$send = mail("voloh@i.ua", $subject, $textMessege, $headers);
  if ($send == 'true'){
 	$responce = 'Ok';
  } else {
